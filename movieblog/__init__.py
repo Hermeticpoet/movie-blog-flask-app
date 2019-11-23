f9f5ac19342962d4a16ba10ce8bba87adc9b4700
Kevin Walton <kevywalton@gmail.com>
2019-11-23 12:52:12 +0000

    Created account page with route and updated navbar links

--- movieblog/__init__.py
+++ movieblog/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "e738627c08889a6b3d91a0c43acd6a16"
-app.config["SQLAlCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+app.config["SQLAlCHEMY_DATABASE_URI"] = "sqlite:///chatter.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from movieblog import routes
--- movieblog/routes.py
+++ movieblog/routes.py
@@ -1,21 +1,22 @@
 from flask import render_template, url_for, flash, redirect
-from movieblog import app
+from movieblog import app, db, bcrypt
 from movieblog.models import User, Post
 from movieblog.forms import RegistrationForm, LoginForm
+from flask_login import login_user, current_user, logout_user
 
 
 posts = [
     {
-        'author': 'Corey Schafer',
+        'author': 'Kevin Walton',
         'title': 'Blog Post 1',
         'content': 'First post content',
-        'date_posted': 'April 20, 2018'
+        'date_posted': 'Sept 20, 2019'
     },
     {
-        'author': 'Jane Doe',
+        'author': 'Aoife Clarke',
         'title': 'Blog Post 2',
         'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
+        'date_posted': 'Oct 21, 2019'
     }
 ]
 
@@ -33,20 +34,43 @@ def about():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f"Account created for { form.username.data }!", "success")
-        return redirect(url_for("home"))
+        # Password Hashing Code
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        user = User(username=form.username.data,
+                    email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f"Your Account has been created!", "success")
+        return redirect(url_for("login"))
     return render_template("register.html", title="Register", form=form)
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == "admin@blog.com" and form.password.data == "password":
-            flash("You have been logged in!", "success")
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
             return redirect(url_for("home"))
         else:
-            flash("Login Unsuccesful. Please check username and password.", "danger")
+            flash("Login Unsuccesful. Please check email and password.", "danger")
     return render_template("login.html", title="Login", form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+
+
+
+@app.route("/account")
+def account():
+    return render_template("account.html", title="Account")
