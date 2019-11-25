9d07a140375be2b20c5dfe5175b475d912db5f7f
Kevin Walton <kevywalton@gmail.com>
2019-11-25 20:01:40 +0000

    Created blueprints & then restructured project directory

--- movieblog/__init__.py
+++ movieblog/__init__.py
@@ -12,7 +12,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = "login"
+login_manager.login_view = "users.login"
 login_manager.login_message_category = "info"
 app.config["MAIL_SERVER"] = "smtp.googlemail.com"
 app.config["PORT"] = 587
@@ -22,4 +22,11 @@ app.config["MAIL_PASSWORD"] = os.environ.get('EMAIL_PASS')
 mail = Mail(app)
 
 
-from movieblog import routes
+from movieblog.users.routes import users
+from movieblog.posts.routes import posts
+from movieblog.main.routes import main
+
+
+app.register_blueprint(users)
+app.register_blueprint(posts)
+app.register_blueprint(main)
