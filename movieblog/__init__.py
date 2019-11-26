a11f4170f8b078687992749560bfd7bb75415c31
Kevin Walton <kevywalton@gmail.com>
2019-11-26 20:04:41 +0000

    Created errors directories with error handling pages

--- movieblog/__init__.py
+++ movieblog/__init__.py
@@ -27,8 +27,11 @@ def create_app(config_class=Config):
     from movieblog.users.routes import users
     from movieblog.posts.routes import posts
     from movieblog.main.routes import main
+    from movieblog.errors.handlers import errors
+
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
